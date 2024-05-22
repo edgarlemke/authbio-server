8d84de5c1f9838cd5de1a653f13a7989afe42dd5
Edgar Roberto Lemke <edgar_lemke@yahoo.com.br>
2024-05-21 23:36:47 -0300

    Correção no start.bash

--- start.bash
+++ start.bash
@@ -2,6 +2,6 @@
 
 mkdir -p log/start
 ./deepface/scripts/dockerize.sh > log/start/deepface-dockerize-stdout 2> log/start/deepface-dockerize-stderr &
-./spring/gradlew bootRun > log/start/spring-stdout 2>log/start/spring-stderr &
 
-
+cd spring
+./gradlew bootRun > ../log/start/spring-stdout 2> ../log/start/spring-stderr &

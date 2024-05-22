a309e2c6b2bfb7ddefe6343d83006e1523a93dae
Edgar Roberto Lemke <edgar_lemke@yahoo.com.br>
2024-05-21 23:22:47 -0300

    Alterado start.bash para criar arquivos de log

--- start.bash
+++ start.bash
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-cd deepface/scripts
-./dockerize.sh &
-cd ..
-
-cd spring
-./gradlew bootRun &
-cd ..
+./deepface/scripts/dockerize.sh > log/start/deepface-dockerize-stdout 2> log/start/deepface-dockerize-stderr &
+./spring/gradlew bootRun > log/start/spring-stdout 2>log/start/spring-stderr &
 
 

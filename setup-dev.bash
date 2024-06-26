82593b83f076637e32ca135bad08e1c28c2315fd
Edgar Roberto Lemke <edgar_lemke@yahoo.com.br>
2024-06-26 20:10:10 -0300

    Added config for allowing root login via ssh to setup-dev.bash

--- setup-dev.bash
+++ setup-dev.bash
@@ -4,3 +4,7 @@ apt update
 apt full-upgrade
 
 apt install git vim ctags nmap --yes
+
+sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
+/etc/init.d/ssh restart
+

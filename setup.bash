655c11d09496cdfc8d8852e6822d2e8a9cb4be99
Edgar Roberto Lemke <edgar_lemke@yahoo.com.br>
2024-06-26 20:13:38 -0300

    Changes on setup.bash, added node comments, node.js setup and creation of svelte project

--- setup.bash
+++ setup.bash
@@ -1,13 +1,28 @@
 #!/bin/bash
 
+# This is the main setup script for getting a running web server instance
+
 apt update
-apt full-upgrade
+apt full-upgrade --yes
 
 apt install sudo mariadb-server --yes
 
 ./setup-docker.bash
 
-# Para dockerizar o deepface e agilizar a inicialização 
+# For dockerizing deepface and speed up initialization
 # cd deepface/scripts/
 # chmod +x ./dockerize.sh
 # ./dockerize.sh
+
+# setup node.js
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo bash -
+apt install nodejs --yes
+
+# install degit
+# npm install -g degit
+
+# create svelte app
+# npx degit sveltejs/template view/svelte
+#cd view/svelte
+#npm install
+#cd ../..
